cfae24ad1c0a30d26b2d5b327c13f2ad6fb13121
Eray Ateş <eray.aates@gmail.com>
2025-09-07 09:08:57 +0300

    Recommended side bar is done. Next step is applying youtube api.

--- youtube-clone/src/Pages/Video/Video.jsx
+++ youtube-clone/src/Pages/Video/Video.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Video.css'
 import PlayerVideo from '../../Components/PlayerVideo/PlayerVideo'
+import Recommended from '../../Components/Recommended/Recommended'
 const Video = () => {
   return (
     <div className='play-container'>
       <PlayerVideo></PlayerVideo>
+      <Recommended></Recommended>
     </div>
   )
 }
